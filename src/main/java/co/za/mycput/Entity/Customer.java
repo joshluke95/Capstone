0ebc3f40f3fe32eafc7c55241f8d7c209e15532f
marcomulonday <219049505@mycput.ac.za>
2022-04-05 18:23:15 +0200

    remove this class from doctor to customer

---
+++ src/main/java/co/za/mycput/Entity/Customer.java
@@ -0,0 +1,121 @@
+package co.za.mycput.Entity;
+/*
+    Author: Tshimanga Marco (219049505)
+
+ */
+public class Customer {
+    private int id;
+    private String custFirstName;
+    private String custLastName;
+    private String custGender;
+    private String custAddress;
+    private long custPhoneNumber;
+
+    public Customer(Builder builder) {
+        this.id = builder.custIdNumber;
+        this.custFirstName = builder.custFirstName;
+        this.custLastName = builder.custLastName;
+        this.custGender = builder.custGender;
+        this.custAddress = builder.custAddress;
+        this.custPhoneNumber = builder.custPhoneNumber;
+    }
+
+
+    public String getCustFirstName() {
+        return custFirstName;
+    }
+
+    public void setCustFirstName(String custFirstName) {
+        this.custFirstName = custFirstName;
+    }
+
+    public String getCustLastName() {
+        return custLastName;
+    }
+
+    public void setCustLastName(String custLastName) {
+        this.custLastName = custLastName;
+    }
+
+    public String getCustGender() {
+        return custGender;
+    }
+
+    public void setCustGender(String custGender) {
+        this.custGender = custGender;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setDrAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    public int getCustIdNumber() {
+        return id;
+    }
+
+    public void setCustIdNumber(int custIdNumber) {
+        this.id = custIdNumber;
+    }
+
+    public long getCustPhoneNumber() {
+        return custPhoneNumber;
+    }
+
+    public void setCustPhoneNumber(long custPhoneNumber) {
+        this.custPhoneNumber = custPhoneNumber;
+    }
+    public static class Builder {
+        private String custFirstName;
+        private String custLastName;
+        private String custGender;
+        private String custAddress;
+        private int custIdNumber;
+        private long custPhoneNumber;
+
+
+        public Builder setCustFirstName(String custFirstName) {
+            this.custFirstName = custFirstName;
+            return this;
+        }
+
+        public Builder setCustLastName(String custLastName) {
+            this.custLastName = custLastName;
+            return this;
+        }
+
+        public Builder setCustGender(String custGender) {
+            this.custGender = custGender;
+            return this;
+        }
+
+        public Builder setCustAddress( String custAddress) {
+            this.custAddress = custAddress;
+            return this;
+        }
+        public Builder setCustIdNumber( int custIdNumber) {
+            this.custIdNumber = custIdNumber;
+            return this;
+        }
+        public Builder setCustPhoneNumber( long custPhoneNumber) {
+            this.custPhoneNumber = custPhoneNumber;
+            return this;
+        }
+        public Builder copy(Customer customer){
+            this.custFirstName = customer.custFirstName;
+            this.custLastName = customer.custLastName;
+            this.custGender = customer.custGender;
+            this.custAddress = customer.custAddress;
+            this.custPhoneNumber = customer.custPhoneNumber;
+            this.custIdNumber = customer.id;
+
+            return this;
+        }
+        public Customer build(){
+            return new Customer(this);
+        }
+    }
+}
