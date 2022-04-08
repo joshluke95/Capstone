ef1e9a78ee9c96eba7fbb0326b249078c7084ee6
joshluke95 <214234169@mycput.ac.za>
2022-04-08 14:44:09 +0200

    Added copywrite comment for DriverFactory class

---
+++ src/main/java/za/ac/mycput/Factory/DriverFactory.java
@@ -0,0 +1,10 @@
+/* DriverFactory.java
+   Factory for the Driver
+   @ author: Joshua Luke Retief (214234169)
+   Date: 08 April 2022
+ */
+
+package za.ac.mycput.Factory;
+
+public class DriverFactory {
+}
