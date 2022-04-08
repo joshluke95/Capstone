228a1a92aa4a4e7f84928cf5695fdab985b665a5
joshluke95 <214234169@mycput.ac.za>
2022-04-08 16:51:06 +0200

    Completed Test Case for DriverFactoryTest

---
+++ src/test/java/za/ac/mycput/Factory/DriverFactoryTest.java
@@ -0,0 +1,24 @@
+/* DriverFactoryTest.java
+   FactoryTest for the DriverFactoryTest
+   @ author: Joshua Luke Retief (214234169)
+   Date: 08 April 2022
+ */
+
+package za.ac.mycput.Factory;
+
+import org.junit.Test;
+import za.ac.mycput.Entity.Driver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DriverFactoryTest {
+    @Test
+    public void test(){
+        Driver driver = DriverFactory.createDriver(214234169, "Joshua","Retief",
+                "064 683 0785");
+        System.out.println(driver.toString());
+        assertNotNull(driver);
+    }
+
+
+}
