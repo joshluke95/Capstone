7571aa9483f2c8c96b3138b655d176c0d59a9a92
joshluke95 <214234169@mycput.ac.za>
2022-04-08 17:02:09 +0200

    Completed Test Case for DeliveryFactoryTest

---
+++ src/test/java/za/ac/mycput/Factory/DeliveryFactoryTest.java
@@ -0,0 +1,25 @@
+/* DeliveryFactoryTest.java
+   FactoryTest for the DeliveryFactoryTest
+   @ author: Joshua Luke Retief (214234169)
+   Date: 08 April 2022
+ */
+
+package za.ac.mycput.Factory;
+
+import org.junit.Test;
+import za.ac.mycput.Entity.Delivery;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeliveryFactoryTest {
+    @Test
+    public void test(){
+        Delivery delivery = new Delivery.BuilderDelivery()
+                .setDeliveryId(94301285)
+                .setDeliveryAddress("463 Crip Walk")
+                .setDeliveryDate("8 April 2022")
+                .build();
+        System.out.println(delivery);
+        assertNotNull(delivery);
+    }
+}
